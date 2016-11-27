2a4033192d1882cedea089e219aa4fe2b9d7a1c7
Emil Tomov <bemcho@gmail.com>
2016-11-27 21:42:10 +0200

    Some clean ups

--- source/main.cpp
+++ source/main.cpp
@@ -21,10 +21,11 @@ CascadeClassifier faceDetect;
 Mat frame, frame_gray;
 
 int main() {
-    VideoCapture capture;
+    VideoCapture capture; //= VideoCapture("/home/bemcho/Movies/wtrust.mkv");
 
     faceDetect.load(face_cascade_name);
-    namedWindow(window_name,WINDOW_OPENGL);
+
+    namedWindow(window_name, WINDOW_OPENGL);
     for (int i = 0; i < 50; i++) {
         capture = VideoCapture(i);
         if (!capture.isOpened()) {
@@ -39,20 +40,19 @@ int main() {
             break;
         }
     }
-
     capture.set(CAP_PROP_FRAME_WIDTH, 10000);
     capture.set(CAP_PROP_FRAME_HEIGHT, 10000);
-
     capture.set(CAP_PROP_FRAME_WIDTH,
                 (capture.get(CAP_PROP_FRAME_WIDTH) / 2) <= 1280 ? 1280 : capture.get(CAP_PROP_FRAME_WIDTH) / 2);
     capture.set(CAP_PROP_FRAME_HEIGHT,
                 (capture.get(CAP_PROP_FRAME_HEIGHT) / 2) <= 720 ? 720 : capture.get(CAP_PROP_FRAME_HEIGHT) / 2);
 
+    int long long fc = 1;
+    vector<Rect> detectsFront, detectsProfile;
     while (true) {
 
-        // create another actor that calls 'hello_world(mirror_actor)';
         capture >> frame;
-
+        fc++;
         cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
         equalizeHist(frame_gray, frame_gray);
 
@@ -61,15 +61,21 @@ int main() {
             break;
         }
 
-        vector<Rect> detects;
-        faceDetect.detectMultiScale(frame_gray,detects);
-
-        for(const auto& rect : detects)
-        {
-            rectangle(frame,rect,CV_RGB(0,255,0),1);
+        /**don't call heavy algorithms 30 or more times per second
+        *call them every 10-th second
+        *just this check gains more than 40% less cpu usage(on my machine debian 8 with I7)
+        * without fc counter was 56% from all  cores with the counter is 13 %
+        **/
+        if (fc % 5 == 0) {
+            detectsFront.clear();
+            faceDetect.detectMultiScale(frame_gray, detectsFront, 1.1, 7, 0 | CASCADE_SCALE_IMAGE);
         }
 
-        imshow(window_name,frame);
+        for (const auto &rect : detectsFront) {
+            rectangle(frame, rect, CV_RGB(0, 125, 255), 2);
+        }
+
+        imshow(window_name, frame);
         //-- bail out if escape was pressed
         if (waitKey(1) == 27) {
             break;
